59cfe494822018cad9a72a01a236411ad55ac181
Daniel Wu <dwubusiness@gmail.com>
2019-06-16 15:07:09 -0700

    6/16/19 refactoring cnn code

--- generate_aws_figures.py
+++ generate_aws_figures.py
@@ -105,7 +105,7 @@ def page_duration():
         plt.savefig("/scratch/PI/euan/projects/mhc/code/daniel_code/aws/figures/duration_{}.png".format(key))
         plt.clf()
 
-def age_activity(healthcode_map):
+def age_activity(healthcode_map, uniq_only=True):
 
     '''Generates a figure and table of the average duration spent on various activities depending on age'''
     
@@ -115,6 +115,7 @@ def age_activity(healthcode_map):
     age_table = pd.read_csv(r'tables/demographics_summary_v2.age.tsv', delimiter='\t').set_index('Subject')
     
     freq = defaultdict(list)
+    uniq = set()
     
     with open(r'/scratch/PI/euan/projects/mhc/code/daniel_code/aws/tables/PageEndedActivities.txt', 'r') as file:
         print("Loading in data")
@@ -122,6 +123,13 @@ def age_activity(healthcode_map):
             data = json.loads(line.rstrip())
             aws_id = data["client"]["client_id"]
             act = data["attributes"]["pageName"]
+            
+            #Include only never before seen ids for each activity
+            if(uniq_only):
+                if((aws_id, act) in uniq):
+                    continue
+                else:
+                    uniq.add((aws_id, act))
             
             #Get the age
             try:
@@ -147,9 +155,161 @@ def age_activity(healthcode_map):
         plt.savefig("/scratch/PI/euan/projects/mhc/code/daniel_code/aws/figures/age_{}.png".format(key))
         plt.clf()
 
+
+
+def usage_graph(healthcode_map):
+
+    '''
+    Generates a figure of overall activities per person
+    Returns healthcodes of the 10 most active people
+    '''
+    
+    usage = defaultdict(int)
+    invalid_aws_id = set()
+    
+    with open(r'/scratch/PI/euan/projects/mhc/code/daniel_code/aws/tables/PageEndedActivities.txt', 'r') as file:
+        print("Loading in data")
+        for line in file:
+            data = json.loads(line.rstrip())
+            aws_id = data["client"]["client_id"]
+            
+
+            #Get the healthcode
+            try:
+                healthcode = healthcode_map[aws_id] 
+                
+                #Count usage
+                usage[healthcode] += 1
+            except KeyError:
+                #print("AWS_id not found {}".format(aws_id))
+                invalid_aws_id.add(aws_id)
+            
+    print("There were {} distinct invalid aws ids".format(len(invalid_aws_id)))
+    print("Finished loading the data - beginning graphing")
+        
+    #Plot everything
+    fig = plt.hist(list(usage.values()))
+    plt.title("Activity of users")
+    plt.xlabel("# activities")
+    plt.ylabel("# users")
+    plt.savefig("/scratch/PI/euan/projects/mhc/code/daniel_code/aws/figures/overall_activity.png")
+    
+    print("Our 10 most active healthcodes were:")
+    most_active = sorted(usage, key=usage.get, reverse=True)[:10]
+    for i in range(10):
+        print("{} with {} activities".format(most_active[i], usage[most_active[i]]))
+
+    return most_active
+
+
+def journey_map(healthcode_map, healthcodes):
+
+    '''
+    Looks at the events and behavior of a specific set of healthcodes
+    '''
+    from datetime import datetime
+
+    events = defaultdict(list)
+    times = defaultdict(list)
+    invalid_aws_id = set()
+    
+    with open(r'/scratch/PI/euan/projects/mhc/code/daniel_code/aws/tables/PageEndedActivities.txt', 'r') as file:
+        print("Loading in data")
+        for line in file:
+            data = json.loads(line.rstrip())
+            aws_id = data["client"]["client_id"]
+            
+
+            #Get the healthcode
+            try:
+                healthcode = healthcode_map[aws_id] 
+                
+                #Store events
+                if healthcode in healthcodes:
+                    act = data["attributes"]["pageName"]
+                    #Convert to seconds instead of milliseconds timestamp
+                    time = datetime.fromtimestamp(data["event_timestamp"]/1000)
+
+                    events[healthcode].append(act[3:-14])
+                    times[healthcode].append(time)
+            except KeyError:
+                invalid_aws_id.add(aws_id)
+            
+    print("There were {} distinct invalid aws ids".format(len(invalid_aws_id)))
+    print("Finished loading the data - beginning graphing")
+    
+    for healthcode in healthcodes:
+        timeline(healthcode, dates = times[healthcode], names = events[healthcode])
+
+
+def timeline(filename, dates, names):
+    
+    '''
+    timeline(filename, dates, names)
+    Function which saves a figure of a timeline of events
+    
+    filename - unique string to append
+    dates - array of datetine objects of each event
+    names - array of labels of events
+    '''
+    
+    import matplotlib.dates as mdates
+    from datetime import datetime
+    
+    print("Saving timeline {}".format(filename))
+    
+    # Choose some nice levels
+    levels = np.tile(np.linspace(-5, 5, num=50),
+                     int(np.ceil(len(dates)/50)))[:len(dates)]
+    
+    # Create figure and plot a stem plot with the date
+    fig, ax = plt.subplots(figsize=(200, 40))
+    ax.set(title="Activity of user {}".format(filename))
+    
+    markerline, stemline, baseline = ax.stem(dates, levels,
+                                             linefmt="C3-", basefmt="k-")
+    
+    plt.setp(markerline, mec="k", mfc="w", zorder=3)
+    
+    # Shift the markers to the baseline by replacing the y-data by zeros.
+    markerline.set_ydata(np.zeros(len(dates)))
+    
+    # annotate lines
+    vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
+    for d, l, r, va in zip(dates, levels, names, vert):
+        ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
+                    textcoords="offset points", va=va, ha="right")
+    
+    # format xaxis with 4 month intervals
+    ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))
+    ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+    
+    # remove y axis and spines
+    ax.get_yaxis().set_visible(False)
+    for spine in ["left", "top", "right"]:
+        ax.spines[spine].set_visible(False)
+    
+    ax.margins(y=0.1)
+    plt.savefig("/scratch/PI/euan/projects/mhc/code/daniel_code/aws/figures/user_timeline_{}.png".format(filename))
+    plt.close()
+    
+    #Plot histogram of dates
+    df = pd.DataFrame(np.ones(len(dates)), pd.DatetimeIndex(dates, name='dates'))
+    fig = df.resample('D').sum().hist()
+    plt.title("Activity of user {}".format(filename))
+    plt.ylabel("# activities")
+    plt.xlabel("Date")
+    plt.savefig("/scratch/PI/euan/projects/mhc/code/daniel_code/aws/figures/user_hist_{}.png".format(filename))
+    plt.close()
+
 if __name__ == '__main__':
     
     healthcode_map = map_aws_to_healthcode()
-    age_activity(healthcode_map)
+    #age_activity(healthcode_map)
     
     #page_duration()
+    
+    most_active = usage_graph(healthcode_map)
+    
+    journey_map(healthcode_map, most_active)
